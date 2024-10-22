bea8f5aee28444de7503c525a447a3f69fcc81ca
BrandonTennant <brtennan@ucsc.edu>
2024-10-22 12:30:00 -0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,16 @@
-def PrintHelloWorld():
-	print("Hello World!")
+def PrintFullName():
+	print("Hello, my name is Brandon Tennant.")
+
+def PrintFavoriteMovie():
+	print("My favorite movie is Howl's Moving Castle, and I love it because it's whimscical nature is eye candy for me.")
+
+def PrintFavoriteFood():
+	print("My favorite food is difficult, because I love eating everything, but if I had to name a favorite, My favorite meals would be all the meals me and my girlfriend cook together.")
 
 def main(): 
-	PrintHelloWorld()
+	PrintFullName()
+	PrintFavoriteMovie()
+	PrintFavoriteFood()
 
 if __name__=="__main__":
 	main()
