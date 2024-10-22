27854d00635fd3b98d5457d9a257283a011aaf4d
BrandonTennant <brtennan@ucsc.edu>
2024-10-22 12:31:02 -0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -5,7 +5,7 @@ def PrintFavoriteMovie():
 	print("My favorite movie is Howl's Moving Castle, and I love it because it's whimscical nature is eye candy for me.")
 
 def PrintFavoriteFood():
-	print("My favorite food is difficult, because I love eating everything, but if I had to name a favorite, My favorite meals would be all the meals me and my girlfriend cook together.")
+	print("My favorite food is difficult to name because I love eating everything, but if I had to name some, my favorite meals would be all the meals me and my girlfriend cook together.")
 
 def main(): 
 	PrintFullName()
